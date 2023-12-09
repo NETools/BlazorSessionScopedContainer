89e4bfdb4e6b6cd6cde77a874b653f8cdc38467a
NETools <117414781+NETools@users.noreply.github.com>
2023-12-09 22:39:37 +0100

    Version 2.0

---
+++ BlazorSessionScopedContainer/Core/SessionManager/MigrationContext/Synchronization/MutexLockObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSessionScopedContainer.Core.SessionManager.MigrationContext.Synchronization
+{
+    public class MutexLockObject : IDisposable
+    {
+        public event Action? Disposed;
+
+        public void Dispose()
+        {
+            Disposed?.Invoke();
+        }
+    }
+}
